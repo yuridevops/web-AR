22adfa22369bbcb6ceba6adc036ddc68f235b8be
yuridevops <yuri.devops@hmail.com>
2020-08-31 15:58:22 -0300

    video

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const path = require('path')
 var fs = require('fs');
+/*
 var http = require('http');
 var https = require('https');
 var privateKey  = fs.readFileSync(path.normalize( __dirname + '/sslcert/server.key'), 'utf8');
 var certificate = fs.readFileSync(path.normalize( __dirname + '/sslcert/server.cert'), 'utf8');
+
 var credentials = {key: privateKey, cert: certificate};
 
-
+*/
 const app = express()
 
 app.use(express.json())
@@ -21,6 +23,8 @@ app.get('/qrcode', (request, response) =>{
   response.sendFile(__dirname + '/templates/ar_sicredi.html')
 } )
 
-var httpsServer = https.createServer(credentials, app);
+/*var httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(8080);
+*/
+app.listen(8080)
